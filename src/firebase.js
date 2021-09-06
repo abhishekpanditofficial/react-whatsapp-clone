315401ba64aaf7cf54d6e7682bae31972417f82f
abhishekpanditofficial <abhishekpanditoff@gmail.com>
2021-09-06 23:43:26 +0530

    updated PI

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   
+    apiKey: "AIzaSyCCvjYFZzLIf6vdyPYBzYNwOv9Fic5-tow",
+    authDomain: "whatsapp-mern-5bcb7.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-5bcb7.firebaseio.com",
+    projectId: "whatsapp-mern-5bcb7",
+    storageBucket: "whatsapp-mern-5bcb7.appspot.com",
+    messagingSenderId: "878115685707",
+    appId: "1:878115685707:web:1a33c97cb7e0ecf06dd4ce",
+    measurementId: "G-BGMS3TL4T8"
   };
